ee9e499a027dde7d2830a6db8d39c8dc33445d67
KIRAGGA <preagkan@gmail.com>
2020-08-27 00:38:44 +0300

    Working on batches

--- resources/views/batches/index.blade.php
+++ resources/views/batches/index.blade.php
@@ -4,7 +4,7 @@
     <section class="content-header">
         <h1 class="pull-left">Batches</h1>
         <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{{ route('batches.create') }}">Add New</a>
+           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" ><i class="fa fa-plus-circle"></i>Add New</a>
         </h1>
     </section>
     <div class="content">
