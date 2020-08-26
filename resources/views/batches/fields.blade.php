f7a6d05a5b2779a107ee0c0b3594b5f706bd8051
KIRAGGA <preagkan@gmail.com>
2020-08-27 01:19:17 +0300

    Working on batches

--- resources/views/batches/fields.blade.php
+++ resources/views/batches/fields.blade.php
@@ -1,11 +1,29 @@
-<!-- Batch Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('batch', 'Batch:') !!}
-    {!! Form::text('batch', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
-</div>
+<!-- Modal -->
+<div class="modal fade" id="batch-add-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle"><i class="fa fa-id-badge" aria-hidden="true"> Add New Batch</i></h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
 
+
+<!-- Batch Field -->
+<div class="input-group col-md-12">
+    <span class="input-group-addon">Batch Year</span>
+    {!! Form::text('year', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+</div>
+        </div>
 <!-- Submit Field -->
-<div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{{ route('batches.index') }}" class="btn btn-default">Cancel</a>
+<div class="modal-footer">
+    <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+    {!! Form::submit('Create Batch', ['class' => 'btn btn-success']) !!}
+    {{-- <a href="{{ route('batches.index') }}" class="btn btn-default">Cancel</a> --}}
+</div>
+        </div>
+      </div>
+    </div>
 </div>
