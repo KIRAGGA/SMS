9f9ce6c72932d2ccca3a643276d2b08793782067
KIRAGGA <preagkan@gmail.com>
2020-08-27 23:28:41 +0300

    Working on days

--- resources/views/days/fields.blade.php
+++ resources/views/days/fields.blade.php
@@ -1,11 +1,27 @@
-<!-- Name Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('name', 'Name:') !!}
-    {!! Form::text('name', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
-</div>
+<!-- Modal -->
+<div class="modal fade left" id="class-add-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-notify modal-ms modal-right modal-success" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add New Day</i></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+                <div class="modal-body">
+
+                    <!-- Name Field -->
+                    <div class="input-group col-md-12">
+                        <span class="input-group-addon">Days</span>
+                        {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Enter Day Here', 'maxlength' => 255,'maxlength' => 255]) !!}
+                    </div>
+                </div>
 
 <!-- Submit Field -->
-<div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{{ route('days.index') }}" class="btn btn-default">Cancel</a>
+<div class="modal-footer">
+    <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+    {!! Form::submit('Create Class', ['class' => 'btn btn-success']) !!}
+</div>
+</div>
+</div>
 </div>
--- resources/views/days/table.blade.php
+++ resources/views/days/table.blade.php
@@ -13,7 +13,12 @@
                 <td>
                     {!! Form::open(['route' => ['days.destroy', $day->id], 'method' => 'delete']) !!}
                     <div class='btn-group'>
-                        <a href="{{ route('days.show', [$day->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
+                        <a data-toggle="modal" data-target="#day-view-modal" data-day="{{$day->name}}"
+                        data-created_at="{{$day->created_at}}" data-updated_at="{{$day->updated_at}}" data-day_id="{{$day->day_id}}"
+                            class='btn btn-default btn-xs'> <i class="glyphicon glyphicon-eye-open"></i></a>
+
+                            
+                        {{-- <a href="{{ route('days.show', [$day->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a> --}}
                         <a href="{{ route('days.edit', [$day->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
                         {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                     </div>
@@ -24,3 +29,67 @@
         </tbody>
     </table>
 </div>
+
+
+<!-- Modal -->
+<div class="modal fade left" id="day-view-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><i class="fa fa-id-badge" aria-hidden="true"></i></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+                <div class="modal-body">
+                    <input type="hidden" name="day_id" id="day_id">
+                    {{-- the hidden id is used to fetch the data --}}
+                    <!-- Year Field -->
+                    <div class="form-group">
+                        {!! Form::label('day', 'Day:') !!}
+                        <input type="text" name="day" id="day" readonly>
+                    </div>
+
+                    <!-- Created at Field -->
+                    <div class="form-group">
+                        {!! Form::label('created_at', 'Created At:') !!}
+                        <input type="text" name="created_at" id="created_at" readonly>
+                    </div>
+
+                    <!-- Updated On Field -->
+                    <div class="form-group">
+                        {!! Form::label('updated_at', 'Updated At:') !!}
+                        <input type="text" name="updated_at" id="Updated_at" readonly>
+                    </div>
+                </div>
+<!-- Submit Field -->
+<div class="modal-footer">
+    <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+    {!! Form::submit('Create Batch', ['class' => 'btn btn-success']) !!}
+    {{-- <a href="{{ route('batches.index') }}" class="btn btn-default">Cancel</a> --}}
+</div>
+        </div>
+      </div>
+    </div>
+</div>
+
+@section('scripts')
+    <script>
+        $('#day-view-modal').on('show.bs.modal', function(event){
+        var button = $(event.relatedTarget)
+        var day = button.data('day')
+        var created_at = button.data('created_at')
+        var updated_at = button.data('updated_at')
+        var day_id = button.data('day_id')
+
+        var modal = (this)
+
+        modal.find('.modal-title').text('VIEW BATCH INFORMATION');
+        modal.find('.modal-body #day').val(day);
+        modal.find('.modal-body #created_at').val(created_at);
+        modal.find('.modal-body #updated_at').val(updated_at);
+        modal.find('.modal-body #day_id').val(day_id);
+        });
+// this is a bootstrap simple code you can read the bootstrap modal.find
+    </script>
+@endsection
