718f146f2d957c6ecbe5e85b36beca5b6fe7c244
KIRAGGA <preagkan@gmail.com>
2020-08-27 23:56:42 +0300

    Working on days

--- resources/views/days/show_fields.blade.php
+++ resources/views/days/show_fields.blade.php
@@ -4,3 +4,14 @@
     <p>{{ $day->name }}</p>
 </div>
 
+<!-- Created At Field -->
+<div class="form-group">
+    {!! Form::label('created_at', 'Updated At:') !!}
+    <p>{{ $day->created_at }}</p>
+</div>
+
+<!-- Updated At Field -->
+<div class="form-group">
+    {!! Form::label('updated_at', 'Updated At:') !!}
+    <p>{{ $day->updated_at }}</p>
+</div>
--- resources/views/days/table.blade.php
+++ resources/views/days/table.blade.php
@@ -11,15 +11,15 @@
             <tr>
             <td>{{$day->name}}</td>
                 <td>
-                    {!! Form::open(['route' => ['days.destroy', $day->id], 'method' => 'delete']) !!}
+                    {{-- {!! Form::open(['route' => ['days.destroy', $day->id], 'method' => 'delete']) !!} --}}
                     <div class='btn-group'>
                         {{-- <a data-toggle="modal" data-target="#day-view-modal" data-day="{{$day->name}}"
                         data-created_at="{{$day->created_at}}" data-updated_at="{{$day->updated_at}}" data-day_id="{{$day->day_id}}"
                             class='btn btn-default btn-xs'> <i class="glyphicon glyphicon-eye-open"></i></a> --}}
 
 
-                        <a href="{{ route('days.show', [$day->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                        <a href="{{ route('days.edit', [$day->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
+                        <a href="{{ route('days.show', [$day->day_id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
+                        <a href="{{ route('days.edit', [$day->day_id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
                         {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                     </div>
                     {!! Form::close() !!}
