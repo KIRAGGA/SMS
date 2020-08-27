995a1328e928fcf6796b29530080b2ed750c998f
KIRAGGA <preagkan@gmail.com>
2020-08-27 15:58:29 +0300

    Working on batches

--- resources/views/batches/fields.blade.php
+++ resources/views/batches/fields.blade.php
@@ -1,14 +1,14 @@
 <!-- Modal -->
-<div class="modal fade" id="batch-add-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle"><i class="fa fa-id-badge" aria-hidden="true"> Add New Batch</i></h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
+<div class="modal fade left" id="batch-add-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-notify modal-ms modal-right modal-success" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><i class="fa fa-id-badge" aria-hidden="true">Add New Batch</i></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+                <div class="modal-body">     
 
 
 <!-- Batch Field -->
--- resources/views/batches/table.blade.php
+++ resources/views/batches/table.blade.php
@@ -2,19 +2,19 @@
     <table class="table" id="batches-table">
         <thead>
             <tr>
-                <th>Batch</th>
+                <th>Year</th>
                 <th colspan="3">Action</th>
             </tr>
         </thead>
         <tbody>
         @foreach($batches as $batch)
             <tr>
-                <td>{{ $batch->batch }}</td>
+                <td>{!! $batch->year !!}</td>
                 <td>
-                    {!! Form::open(['route' => ['batches.destroy', $batch->id], 'method' => 'delete']) !!}
+                    {{-- {!! Form::open(['route' => ['batches.destroy', $batch->id], 'method' => 'delete']) !!} --}}
                     <div class='btn-group'>
-                        <a href="{{ route('batches.show', [$batch->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                        <a href="{{ route('batches.edit', [$batch->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
+                        <a href="{{ route('batches.show', [$batch->batch_id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
+                        <a href="{{ route('batches.edit', [$batch->batch_id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
                         {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                     </div>
                     {!! Form::close() !!}
@@ -24,3 +24,25 @@
         </tbody>
     </table>
 </div>
+
+@section('name')
+    <script>
+        $('#level-show').on('show.bs.modal', function(event){
+        var button = $(event.relatedTarget)
+        var level = button.data('level')
+        var course_id = button.data('course_id')
+        var level_description = button.data('level_description')
+        var created_at = button.data('created_at')
+        var updated_at = button.data('updated_at')
+        var level_id = button.data(;leel_id)
+
+        var modal = (this)
+
+        modal.find('modal-title').text('VIEW LEVEL INFORMATION');
+        modal.find('modal-body #level').val(level);
+        modal.find('modal-body #course_id').val(course_id);
+        modal.find('modal-body #level_description').cal(level_description);
+        });
+// this is a bootstrap simple code you can read the bootstrap modal.find
+    </script>
+@endsection
