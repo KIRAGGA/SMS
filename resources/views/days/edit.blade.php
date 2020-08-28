324bdd1ad4138a1d0d05b6a8a12dea221510f4f9
KIRAGGA <preagkan@gmail.com>
2020-08-28 13:40:21 +0300

    Working on days

--- resources/views/days/edit.blade.php
+++ resources/views/days/edit.blade.php
@@ -13,7 +13,13 @@
                <div class="row">
                    {!! Form::model($day, ['route' => ['days.update', $day->day_id], 'method' => 'patch']) !!}
 
-                        @include('days.fields')
+                    <div class="form-group col-md-12">
+                        <span class="input-group-addon">Days</span>
+                        {!! Form::text('name', null, ['form-control', 'placeholder' => 'Enter Day Here', 'maxlength' => 255,'maxlength' => 255]) !!}
+                    </div>
+                    <div class="modal-footer">
+                        {!! Form::submit('Update Days', ['class' => 'btn btn-info'])!!}
+                    </div>
 
                    {!! Form::close() !!}
                </div>
