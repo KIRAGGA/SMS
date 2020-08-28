482e9bc894e2d2f621507a963ad4123d3dac828c
KIRAGGA <preagkan@gmail.com>
2020-08-28 16:43:40 +0300

    Working on classrooms

--- resources/views/classrooms/index.blade.php
+++ resources/views/classrooms/index.blade.php
@@ -4,7 +4,8 @@
     <section class="content-header">
         <h1 class="pull-left">Classrooms</h1>
         <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px"><i class="fa fa-plus-circle"></i>Add New</a>
+           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" 
+           data-toggle="modal" data-target="#classroom-add-modal"><i class="fa fa-plus-circle">Add New Class</i></a>
         </h1>
     </section>
     <div class="content">
@@ -15,6 +16,11 @@
         <div class="clearfix"></div>
         <div class="box box-primary">
             <div class="box-body">
+                {!! Form::open(['route' => 'classrooms.store']) !!}
+
+                        @include('classrooms.fields')
+
+                    {!! Form::close() !!}
                     @include('classrooms.table')
             </div>
         </div>
--- resources/views/classrooms/table.blade.php
+++ resources/views/classrooms/table.blade.php
@@ -18,10 +18,10 @@
             <td>{{ $classroom->classroom_description }}</td>
             <td>{{ $classroom->classroom_status }}</td>
                 <td>
-                    {!! Form::open(['route' => ['classrooms.destroy', $classroom->id], 'method' => 'delete']) !!}
+                    {!! Form::open(['route' => ['classrooms.destroy', $classroom->classroom_id], 'method' => 'delete']) !!}
                     <div class='btn-group'>
-                        <a href="{{ route('classrooms.show', [$classroom->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                        <a href="{{ route('classrooms.edit', [$classroom->id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
+                        <a href="{{ route('classrooms.show', [$classroom->classroom_id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
+                        <a href="{{ route('classrooms.edit', [$classroom->classroom_id]) }}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
                         {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                     </div>
                     {!! Form::close() !!}
@@ -31,3 +31,91 @@
         </tbody>
     </table>
 </div>
+
+
+<!-- Modal -->
+<div class="modal fade left" id="day-view-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><i class="fa fa-id-badge" aria-hidden="true"></i></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+                <div class="modal-body">
+                    <input type="hidden" name="classroom_id" id="classroom_id">
+                    {{-- the hidden id is used to fetch the data --}}
+                    <!-- Classroom name Field -->
+                    <div class="form-group">
+                        {!! Form::label('classroom_name', 'Classroom Name:') !!}
+                        <input type="text" name="classroom_name" id="Classroom_name" readonly>
+                    </div>
+
+                    <!-- Classcode Name Field -->
+                    <div class="form-group">
+                        {!! Form::label('classcode_name', 'Classcode Name:') !!}
+                        <input type="text" name="classcode_name" id="classcode_name" readonly>
+                    </div>
+
+                    <!-- Classroom description Field -->
+                    <div class="form-group">
+                        {!! Form::label('classroom_description', 'Classroom Description:') !!}
+                        <input type="text" name="classroom_description" id="classroom_description" readonly>
+                    </div>
+
+                    <!-- Classroom status Field -->
+                    <div class="form-group">
+                        {!! Form::label('classroom_status', 'Classroom Description:') !!}
+                        <input type="text" name="classroom_status" id="classroom_status" readonly>
+                    </div>
+
+                    <!-- Created at Field -->
+                    <div class="form-group">
+                        {!! Form::label('created_at', 'Created At:') !!}
+                        <input type="text" name="created_at" id="created_at" readonly>
+                    </div>
+
+                    <!-- Updated On Field -->
+                    <div class="form-group">
+                        {!! Form::label('updated_at', 'Updated At:') !!}
+                        <input type="text" name="updated_at" id="Updated_at" readonly>
+                    </div>
+                </div>
+<!-- Submit Field -->
+<div class="modal-footer">
+    <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+    {!! Form::submit('Create Batch', ['class' => 'btn btn-success']) !!}
+    {{-- <a href="{{ route('batches.index') }}" class="btn btn-default">Cancel</a> --}}
+</div>
+        </div>
+      </div>
+    </div>
+</div>
+
+@section('scripts')
+    <script>
+        $('#day-view-modal').on('show.bs.modal', function(event){
+        var button = $(event.relatedTarget)
+        var classcode_name = button.data('classroom_name')
+        var classcode_name = button.data('classcode_name')
+        var classroom_description = button.data('classroom_description')
+        var classroom_status = button.data('classroom_status')
+        var created_at = button.data('created_at')
+        var updated_at = button.data('updated_at')
+        var day_id = button.data('classroom_id')
+
+        var modal = (this)
+
+        modal.find('.modal-title').text('VIEW CLASSROOM INFORMATION');
+        modal.find('.modal-body #classroom_name').val(class_name);
+        modal.find('.modal-body #classcode_name').val(classcode_name);
+        modal.find('.modal-body #classroom_description').val(classroom_description);
+        modal.find('.modal-body #classroom_status').val(classroom_status);
+        modal.find('.modal-body #created_at').val(created_at);
+        modal.find('.modal-body #updated_at').val(updated_at);
+        modal.find('.modal-body #classroom_id').val(classroom_id);
+        });
+// this is a bootstrap simple code you can read the bootstrap modal.find
+    </script>
+@endsection
