f6446bf5f652edeb61726141e7605b680c1f7aa1
KIRAGGA <preagkan@gmail.com>
2020-08-29 01:17:17 +0300

    Working on multiusers

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,4 +1,4 @@
-<li class="treeview">
+@if(Auth::user()->role_id < 2 )<li class="treeview">
     <a href="#">
         <i class="fa fa-dashboard"></i>
         <span>General</span>
@@ -50,6 +50,7 @@
 
     </ul>
 </li>
+
 <li class="treeview">
     <a href="#">
         <i class="fa fa-dashboard"></i>
@@ -68,6 +69,7 @@
         </li>
     </ul>
 </li>
+@endif
 
 <li class="{{ Request::is('admissions*') ? 'active' : '' }}">
     <a href="{{ route('admissions.index') }}"><i class="fa fa-user"></i><span>Admissions</span></a>
@@ -81,7 +83,7 @@
     <a href="{{ route('users.index') }}"><i class="fa fa-user"></i><span>Users</span></a>
 </li>
 
-@if(Auth::user()->role_id < 4)
+@if(Auth::user()->role_id < 2 )
 <li class="{{ Request::is('attendances*') ? 'active' : '' }}">
     <a href="{{ route('attendances.index') }}"><i class="fa fa-calendar"></i><span>Attendances</span></a>
 </li>
@@ -93,7 +95,7 @@
 
 
 
-@if(Auth::user()->role_id < 4)
+@if(Auth::user()->role_id < 2 )
 <li class="{{ Request::is('transactions*') ? 'active' : '' }}">
     <a href="{{ route('transactions.index') }}"><i class="fa fa-money"></i><span>Transactions</span></a>
 </li>
