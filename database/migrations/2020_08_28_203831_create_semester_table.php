8ddb6f48f4de76f771111a67ce2224d08534963e
KIRAGGA <preagkan@gmail.com>
2020-08-28 23:58:47 +0300

    Working on academics

---
+++ database/migrations/2020_08_28_203831_create_semester_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSemesterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('semester', function (Blueprint $table) {
+            $table->bigIncrements('semester_id');
+            $table->string('semester_name');
+            $table->string('semester_code')->unique();
+            $table->string('semester_duration');
+            $table->longText('semester_description');
+            // $table->tinyInteger('faculty_status')->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('semester');
+    }
+}
