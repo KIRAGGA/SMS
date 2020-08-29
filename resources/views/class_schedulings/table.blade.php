e7ad3e318d719427da55cf2901de2ece94514e13
KIRAGGA <preagkan@gmail.com>
2020-08-29 23:59:25 +0300

    working on class_schedule

--- resources/views/class_schedulings/table.blade.php
+++ resources/views/class_schedulings/table.blade.php
@@ -2,14 +2,17 @@
     <table class="table" id="classSchedulings-table">
         <thead>
             <tr>
-                <th>Course Id</th>
-        <th>Level Id</th>
-        <th>Shift Id</th>
-        <th>Classroom Id</th>
-        <th>Batch Id</th>
-        <th>Day Id</th>
-        <th>Time Id</th>
-        <th>Teacher Id</th>
+                <th>Course</th>
+        <th>Level</th>
+        <th>Shift</th>
+        <th>Classroom</th>
+        <th>Batch </th>
+        <th>Day </th>
+        <th>Time </th>
+        <th>Teacher </th>
+        <th>Classroom</th>
+        <th>Shifts</th>
+        <th>Semester</th>
         <th>Start Time</th>
         <th>End Time</th>
         <th>Status</th>
@@ -20,16 +23,22 @@
         @foreach($classSchedulings as $classScheduling)
             <tr>
                 <td>{{ $classScheduling->course_id }}</td>
-            <td>{{ $classScheduling->level_id }}</td>
-            <td>{{ $classScheduling->shift_id }}</td>
-            <td>{{ $classScheduling->classroom_id }}</td>
-            <td>{{ $classScheduling->batch_id }}</td>
-            <td>{{ $classScheduling->day_id }}</td>
-            <td>{{ $classScheduling->time_id }}</td>
+            <td>{{ $classScheduling->level }}</td>
+            <td>{{ $classScheduling->shift_name }}</td>
+            <td>{{ $classScheduling->classroom_name }}</td>
+            <td>{{ $classScheduling->year }}</td>
+            <td>{{ $classScheduling->name }}</td>
+            <td>{{ $classScheduling->time }}</td>
             <td>{{ $classScheduling->teacher_id }}</td>
+            <td>{{ $classScheduling->semester_code }}</td>
             <td>{{ $classScheduling->start_time }}</td>
             <td>{{ $classScheduling->end_time }}</td>
-            <td>{{ $classScheduling->status }}</td>
+            <td>@if{{ $classScheduling->status ==1 }}
+                <div class="color: green">Active</div>
+                @else
+                <div class="color: red">In Active</div>
+                @endif
+            </td>
                 <td>
                     {{-- {!! Form::open(['route' => ['classSchedulings.destroy', $classScheduling->schedule_id], 'method' => 'delete']) !!} --}}
                     <div class='btn-group'>
