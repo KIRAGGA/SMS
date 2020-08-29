cb012bbbed4f84ff166862e27499d4f5c8233430
KIRAGGA <preagkan@gmail.com>
2020-08-30 00:20:13 +0300

    working on level

--- resources/views/levels/fields.blade.php
+++ resources/views/levels/fields.blade.php
@@ -1,23 +1,44 @@
-<!-- Level Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('level', 'Level:') !!}
-    {!! Form::text('level', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
-</div>
+<!-- Modal -->
+<div class="modal fade left" id="level-add-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Add New Classroom</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+                <div class="modal-body">
 
-<!-- Course Id Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('course_id', 'Course Id:') !!}
-    {!! Form::number('course_id', null, ['class' => 'form-control']) !!}
-</div>
+                    <!-- Level Field -->
+                    <div class="form-group col-sm-6">
+                        {!! Form::label('level', 'Level:') !!}
+                        {!! Form::text('level', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+                    </div>
 
-<!-- Level Description Field -->
-<div class="form-group col-sm-12 col-lg-12">
-    {!! Form::label('level_description', 'Level Description:') !!}
-    {!! Form::textarea('level_description', null, ['class' => 'form-control']) !!}
-</div>
+                    <!-- Course Id Field -->
+                    <div class="form-group col-sm-6">
+                        {!! Form::label('course_id', 'Course Id:') !!}
+                        {!! Form::number('course_id', null, ['class' => 'form-control']) !!}
+                    </div>
 
-<!-- Submit Field -->
-<div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{{ route('levels.index') }}" class="btn btn-default">Cancel</a>
+                    <!-- Level Description Field -->
+                    <div class="form-group col-sm-12 col-lg-12">
+                        {!! Form::label('level_description', 'Level Description:') !!}
+                        {!! Form::textarea('level_description', null, ['class' => 'form-control']) !!}
+                    </div>
+                </div>
+                    
+                <!-- Submit Field -->
+                    {{-- <div class="form-group col-sm-12">
+                        {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+                        <a href="{{ route('levels.index') }}" class="btn btn-default">Cancel</a>
+                    </div> --}}
+                <!-- Submit Field -->
+                <div class="modal-footer">
+                    <a href="{{ route('levels.index') }}" class="btn btn-default">Cancel</a>
+                    {!! Form::submit('Create classroom', ['class' => 'btn btn-success']) !!}
+                </div>
+        </div>
+    </div>
 </div>
--- resources/views/levels/index.blade.php
+++ resources/views/levels/index.blade.php
@@ -5,7 +5,7 @@
         <h1 class="pull-left">Levels</h1>
         <h1 class="pull-right">
            <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" 
-           data-toggle="modal" data-target="#classroom-add-modal"><i class="fa fa-plus-circle">Add New Level</i></a>
+           data-toggle="modal" data-target="#level-add-modal"><i class="fa fa-plus-circle">Add New Level</i></a>
         </h1>
     </section>
     <div class="content">
