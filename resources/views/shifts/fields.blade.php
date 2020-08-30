76a6ef5ffb025010efa8ab8f4f300f3dc5666d1d
KIRAGGA <preagkan@gmail.com>
2020-08-30 19:46:02 +0300

    working on shift

--- resources/views/shifts/fields.blade.php
+++ resources/views/shifts/fields.blade.php
@@ -1,11 +1,25 @@
-<!-- Shift Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('shift', 'Shift:') !!}
-    {!! Form::text('shift', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
-</div>
-
-<!-- Submit Field -->
-<div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{{ route('shifts.index') }}" class="btn btn-default">Cancel</a>
-</div>
+<!-- Modal -->
+<div class="modal fade left" id="class-shift-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-notify modal-ms modal-right modal-success" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><i class="fa fa-id-badge" aria-hidden="true">Add New shift</i></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+                <div class="modal-body">
+                    <!-- Shift Field -->
+                    <div class="form-group col-sm-6">
+                        {!! Form::label('shift', 'Shift:') !!}
+                        {!! Form::text('shift', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+                    </div>
+                </div>
+                    <!-- Submit Field -->
+                    <div class="form-group col-sm-12">
+                        {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
+                        <a href="{{ route('shifts.index') }}" class="btn btn-default">Cancel</a>
+                    </div>
+            </div>
+        </div>
+    </div>
