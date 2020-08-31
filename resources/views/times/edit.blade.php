e69359d7c338342a841ad8f0e963b86411569a44
KIRAGGA <preagkan@gmail.com>
2020-08-31 20:06:51 +0300

    working on time

--- resources/views/times/edit.blade.php
+++ resources/views/times/edit.blade.php
@@ -13,7 +13,17 @@
                <div class="row">
                    {!! Form::model($time, ['route' => ['times.update', $time->time_id], 'method' => 'patch']) !!}
 
-                        @include('times.fields')
+                        <!-- Time Field -->
+                        <div class="form-group col-md-8">
+                            {{-- <span class="input-group-addon">Class Code</span> --}}
+                            {!! Form::text('time', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!}
+                        </div>
+
+                        <!-- Submit Field -->
+                        <div class="modal-footer">
+                            {!! Form::submit('Updated Time', ['class' => 'btn btn-success']) !!}
+                            <a href="{{ route('times.index') }}" class="btn btn-default">Cancel</a>
+                        </div>
 
                    {!! Form::close() !!}
                </div>
