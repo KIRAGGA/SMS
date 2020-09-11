7242024103c51b07b709adb956359636ca9df6dc
KIRAGGA <preagkan@gmail.com>
2020-09-11 15:14:48 +0300

    working on schedule

--- resources/views/class_schedulings/fields.blade.php
+++ resources/views/class_schedulings/fields.blade.php
@@ -1,5 +1,6 @@
 <!-- Modal -->
-<div class="modal fade" id="classschedule-show" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="classschedule-show" tabindex="-1" role="dialog" 
+aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" style="width:90%">
         <div class="modal-content">
             <div class="modal-header">
