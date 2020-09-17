4bb708d84c1a9eea4be6325f6451581e9d4b42ae
KIRAGGA <preagkan@gmail.com>
2020-09-17 19:52:42 +0300

    working on teacher's

--- resources/views/teachers/index.blade.php
+++ resources/views/teachers/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <section class="content-header">
-        <h1 class="pull-left">Teachers</h1>
+        <h1 class="pull-left"><i class="fa fa-book"></i> Teachers</h1>
         <h1 class="pull-right">
            <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{{ route('teachers.create') }}"><i class="fa fa-plus-circle"></i>Add New Teacher</a>
         </h1>
