3802f1e836520a4f192b616cf2bb901f162571ca
KIRAGGA <preagkan@gmail.com>
2020-09-28 20:25:07 +0300

    working on admission

--- resources/views/admissions/fields.blade.php
+++ resources/views/admissions/fields.blade.php
@@ -189,6 +189,7 @@
                     <!-- Image Field -->
                     <div class="form-group col-sm-6">
                         {!! Form::label('image', 'Image:') !!}
+                        {!!Html::image('teacher_images/profile.jpg',null,['class'=>'teacher-image', 'id'=>'showImage'])!!}
                         {{-- {!! Form::text('image', null, ['class' => 'form-control','maxlength' => 255,'maxlength' => 255]) !!} --}}
                         <input type="file" name="image" id="image" accept="image/x-png,image/png,image/jpg,image/jpeg">
                     </div>
