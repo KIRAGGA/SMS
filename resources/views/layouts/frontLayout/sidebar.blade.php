d6505f0b6b60717a6adc4af72d9c56b75914778c
KIRAGGA <preagkan@gmail.com>
2020-10-01 17:55:56 +0300

    working on student logs

--- resources/views/layouts/frontLayout/sidebar.blade.php
+++ resources/views/layouts/frontLayout/sidebar.blade.php
@@ -1,9 +1,9 @@
   <!-- Left side column. contains the logo and sidebar -->
-  <aside class="main-sidebar">
+  {{-- <aside class="main-sidebar"> --}}
     <!-- sidebar: style can be found in sidebar.less -->
-    <section class="sidebar">
+    {{-- <section class="sidebar"> --}}
       <!-- Sidebar user panel -->
-      <div class="user-panel">
+      {{-- <div class="user-panel">
         <div class="pull-left image">
           <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
         </div>
@@ -11,9 +11,29 @@
           <p>Alexander Pierce</p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
-      </div>
+      </div> --}}
+
+      {{-- This code is pasted --}}
+      {{-- <div class="user-panel">
+        <div class="pull-left image">
+            <img src="http://infyom.com/images/logo/blue_logo_150x150.jpg" class="img-circle"
+                 alt="User Image"/>
+        </div>
+        <div class="pull-left info">
+            @if (Auth::guest())
+            <p>KIFLAPS</p>
+            @else
+                <p>{{ Auth::user()->name}}</p>
+            @endif
+            <!-- Status -->
+            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+    </div> --}}
+
+
+    
       <!-- search form -->
-      <form action="#" method="get" class="sidebar-form">
+      {{-- <form action="#" method="get" class="sidebar-form">
         <div class="input-group">
           <input type="text" name="q" class="form-control" placeholder="Search...">
           <span class="input-group-btn">
@@ -22,10 +42,10 @@
                 </button>
               </span>
         </div>
-      </form>
+      </form> --}}
       <!-- /.search form -->
       <!-- sidebar menu: : style can be found in sidebar.less -->
-      <ul class="sidebar-menu" data-widget="tree">
+      {{-- <ul class="sidebar-menu" data-widget="tree">
         <li class="header">MAIN NAVIGATION</li>
         <li class="active treeview menu-open">
           <a href="#">
@@ -196,6 +216,49 @@
         <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
         <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
       </ul>
+    </section> --}}
+    <!-- /.sidebar -->
+  {{-- </aside> --}}
+
+
+  <aside class="main-sidebar" id="sidebar-wrapper">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="http://infyom.com/images/logo/blue_logo_150x150.jpg" class="img-circle"
+                     alt="User Image"/>
+            </div>
+            <div class="pull-left info">
+                @if (Auth::guest())
+                <p>KIFLAPS</p>
+                @else
+                    <p>{{ Auth::user()->name}}</p>
+                @endif
+                <!-- Status -->
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+
+        <!-- search form (Optional) -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search..."/>
+          <span class="input-group-btn">
+            <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
+            </button>
+          </span>
+            </div>
+        </form>
+        <!-- Sidebar Menu -->
+
+        <ul class="sidebar-menu" data-widget="tree">
+            @include('layouts.menu')
+        </ul>
+        <!-- /.sidebar-menu -->
     </section>
     <!-- /.sidebar -->
-  </aside>
+</aside>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -18,7 +18,20 @@
                 <!-- Status -->
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
-        </div>
+        </div>    <!-- jQuery 3.1.1 -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+        <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.3/js/adminlte.min.js"></script>
+    
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.min.js"></script>
+    
+        @stack('scripts')
+    </body>
 
         <!-- search form (Optional) -->
         <form action="#" method="get" class="sidebar-form">
